e605ab5797db25e35cd7066b047bb5867d4c7b34
Fabien LALLEMAND <fabien.lallemand@d2-si.eu>
2017-11-15 14:05:20 +0100

    add ASG

--- webapp/prod/main.tf
+++ webapp/prod/main.tf
@@ -1,10 +1,8 @@
 provider "aws" {
-  region = "eu-west-1"
 }
 
-variable environment {
-  default = "dev"
-}
+# Create a new instance of the latest Ubuntu 14.04 on an
+# t2.micro node with an AWS Tag naming it "HelloWorld"
 
 data "aws_ami" "ubuntu" {
   most_recent = true
@@ -22,31 +20,66 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-data template_file "userdata" {
+resource "aws_elb" "web_elb" {
+  name            = "web-elb"
+  subnets         = ["${data.terraform_remote_state.coreinfra.subnets}"]
+  security_groups = ["${aws_security_group.allow_ssh.id}"]
+
+  ## Loadbalancer configuration
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 2
+    target              = "HTTP:80/"
+    interval            = 5
+  }
+}
+
+resource "aws_launch_configuration" "web" {
+  name          = "web_server_config"
+  image_id      = "${data.aws_ami.ubuntu.id}"
+  instance_type = "t2.micro"
+  user_data              = "${data.template_file.webapp.rendered}"
+  security_groups        = ["${aws_security_group.allow_ssh.id}"]
+  key_name               = "MY_KEY"
+}
+
+
+resource "aws_autoscaling_group" "web" {
+  name                 = "asb-web"
+  launch_configuration = "${aws_launch_configuration.web.name}"
+  load_balancers       = ["${aws_elb.web_elb.id}"]
+  vpc_zone_identifier = ["${data.terraform_remote_state.coreinfra.subnets}"]
+  min_size             = 2
+  max_size             = 2
+  health_check_type = "EC2"
+  health_check_grace_period = "300"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+data template_file "webapp" {
   template = "${file("${path.module}/userdata.tpl")}"
 
   vars {
-    username = "Fab"
+    username = "JE SUIS USER"
   }
 }
 
-resource "aws_instance" "web" {
-  ami                    = "${data.aws_ami.ubuntu.id}"
-  instance_type          = "t2.micro"
-  key_name               = "gitD2SI"
-  vpc_security_group_ids = ["${aws_security_group.web.id}"]
-  subnet_id              = "${element(data.terraform_remote_state.coreInfra.subnets,0)}"
-  user_data              = "${data.template_file.userdata.rendered}"
-
-  tags {
-    Name = "webapp-${var.environment}"
-  }
-}
-
-resource "aws_security_group" "web" {
-  name        = "ssh_and_http"
-  description = "Allow inbound traffic"
-  vpc_id      = "${data.terraform_remote_state.coreInfra.vpc_id}"
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_all"
+  description = "Allow ssh trafic"
+  vpc_id      = "${data.terraform_remote_state.coreinfra.vpc_id}"
 
   ingress {
     from_port   = 22
@@ -69,3 +102,8 @@ resource "aws_security_group" "web" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+output "elb_dns" {
+  value = ["${aws_elb.web_elb.dns_name}"]
+}
+
--- webapp/prod/remote.tf
+++ webapp/prod/remote.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "flams-admin"
-    key    = "d2si/lab/webapp-lab.tfstate"
+    bucket = "mon-bucket-name"
+    key    = "lab/webapp.tfstate"
     region = "eu-west-1"
   }
 }
@@ -10,8 +10,8 @@ data "terraform_remote_state" "coreInfra" {
   backend = "s3"
 
   config {
-    bucket = "flams-admin"
-    key    = "d2si/lab/coreInfra.tfstate"
+    bucket = "mon-bucket-name"
+    key    = "lab/coreInfra.tfstate"
     region = "eu-west-1"
   }
 }
