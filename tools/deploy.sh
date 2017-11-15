529632ec55fafb84edcb49edeaed6b719b2b5375
delosric <delosric@yahoo.com>
2017-11-15 18:33:27 +0100

    test7

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -4,7 +4,7 @@ cd $TRAVIS_BUILD_DIR/packer
 packer build webapp.json | tee /tmp/packer.out
 AMI=$(awk -F':' '/(eu|us|ap|sa)-(west|central|east|northeast|southeast)-(1|2): ami-/ {print $2}' /tmp/packer.out |  tr -d '[[:space:]]')
 
-cd $TRAVIS_BUILD_DIR/webapp
+cd $TRAVIS_BUILD_DIR/webapp/prod
 terraform init -var ami_id=$AMI
 terraform plan -var ami_id=$AMI
 terraform apply -var ami_id=$AMI
